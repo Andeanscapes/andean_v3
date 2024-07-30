5646a96ea9be2154c94e2ebeaac9c1be572ed9bd
Heinner Yesid Zapata <hzapatae@Heinners-MacBook-Pro.local>
2024-07-30 13:23:26 -0600

    feat: ✨ Adding basic images

--- src/constant/HomeDark/HeroDataTwo.js
+++ src/constant/HomeDark/HeroDataTwo.js
@@ -2,19 +2,37 @@ const HeroDataTwo = {
     slides : [
         {
             id: '1',
-            imgUrl: '/assets/images/hero/h2.webp',
+            imgUrl: '/assets/images/hero/h1.webp',
             title: 'Your Adventure Travel Expert in Paris',
             sub_title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
         },
         {
             id: '2',
-            imgUrl: '/assets/images/hero/h3.webp',
+            imgUrl: '/assets/images/hero/h2.webp',
             title: 'Your Adventure Travel Expert in Paris',
             sub_title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
         },
         {
             id: '3',
-            imgUrl: '/assets/images/hero/h1.webp',
+            imgUrl: '/assets/images/hero/h3.webp',
+            title: 'Your Adventure Travel Expert in Paris',
+            sub_title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+        },
+        {
+            id: '4',
+            imgUrl: '/assets/images/hero/h4.webp',
+            title: 'Your Adventure Travel Expert in Paris',
+            sub_title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+        },
+        {
+            id: '5',
+            imgUrl: '/assets/images/hero/h5.webp',
+            title: 'Your Adventure Travel Expert in Paris',
+            sub_title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+        },
+        {
+            id: '6',
+            imgUrl: '/assets/images/hero/h6.webp',
             title: 'Your Adventure Travel Expert in Paris',
             sub_title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
         },
